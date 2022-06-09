96c6ade89ba6591cf301f42a6d4fea313834818c
PratikBane <74564886+PratikBane@users.noreply.github.com>
2022-06-09 13:30:43 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 
-GET https://github.com/PratikBane/webhooks?
+GET https://pratikbane.github.io/waweebhook/webhooks?
   hub.mode=subscribe&
   hub.challenge=1158201444&
   hub.verify_token=verifymewebhook
 
   POST / HTTPS/1.1
-Host: github.com/PratikBane/webhooks
+Host: pratikbane.github.io/waweebhook/webhooks
 Content-Type: application/json
 X-Hub-Signature: sha256={super-long-SHA256-signature}
 Content-Length: 311
