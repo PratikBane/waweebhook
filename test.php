25e873e3e67e6968d004796225f66e9517bc7e4c
PratikBane <74564886+PratikBane@users.noreply.github.com>
2022-06-09 13:06:00 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+
+GET https://silicomolds.in/webhooks?
+  hub.mode=subscribe&
+  hub.challenge=1158201444&
+  hub.verify_token=verifymewebhook
+
+  POST / HTTPS/1.1
+Host: silicomolds.in/webhooks
+Content-Type: application/json
+X-Hub-Signature: sha256={super-long-SHA256-signature}
+Content-Length: 311
+
+{
+  "entry": [
+    {
+      "time": 1520383571,
+      "changes": [
+        {
+          "field": "photos",
+          "value":
+            {
+              "verb": "update",
+              "object_id": "10211885744794461"
+            }
+        }
+      ],
+      "id": "10210299214172187",
+      "uid": "10210299214172187"
+    }
+  ],
+  "object": "user"
+}
